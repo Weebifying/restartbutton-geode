63c118c9d4ccad388d3c2e447cb6a7f8ba80780e
Weebify <weebifylmao123@gmail.com>
2024-01-24 18:40:13 +0700

    fixed everything AND added close button to android

--- src/main.cpp
+++ src/main.cpp
@@ -37,18 +37,6 @@ class $modify(AltMenuLayer, MenuLayer) {
 		res_btn->setPosition(16.1, -19.375);
 		res_menu->addChild(res_btn);
 
-		// #ifdef GEODE_IS_ANDROID
-		// 	auto cls_spr = CCSprite::createWithSpriteFrameName("GJ_closeBtn_001.png");
-		// 	cls_spr->setScale(0.7);
-		// 	auto cls_btn = CCMenuItemSpriteExtra::create(
-		// 		cls_spr, 
-		// 		this, 
-		// 		menu_selector(MenuLayer::onQuit));
-		// 	cls_btn->setContentSize(CCSize(32.2, 33.25));
-		// 	cls_btn->setPosition(16.1, 16.625);
-		// 	res_menu->addChild(cls_btn);
-		// #endif
-
 		return true;
 	}
 
